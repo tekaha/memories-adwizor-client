2cff8c5e2abf74c37ef74c85b1ee943f47be25e9
Holger Wingold <holger.wingold@adwizor.de>
2022-11-23 16:55:05 +0100

    Prepare for Cyclic

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const API = axios.create({ baseURL: 'http://localhost:5000' })
-const API = axios.create({ baseURL: 'https://memories-adwizor.herokuapp.com' })
+const API = axios.create({ baseURL: 'https://memories-adwizor-backend.cyclic.app/' })
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
